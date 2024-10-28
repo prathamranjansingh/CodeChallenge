8730781f3e6dbb82813d2ce1a4d741c65dd18725
prathamranjansingh <prathamranjan3@gmail.com>
2024-10-28 12:38:02 +0530

    navbar dev

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,44 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
+import { useEffect, useState } from "react";
 
 export const API_URL = "http://localhost:5040";
+export const TOKEN_STORAGE_KEY = "id";
+export const ID_STORAGE_KEY="id";
+
 const App = () => {
+  const [token, setToken] = useState(localStorage.getItem(TOKEN_STORAGE_KEY));
+  const [id, setId] = useState(localStorage.getItem(ID_STORAGE_KEY));
+
+
+  const changeToken = (string: string) => {
+    setToken(string);
+  };
+  const changeId = (string: string) => {
+      setId(string);
+  };
+
+  useEffect(()=>{
+    if(token){
+      localStorage.setItem(TOKEN_STORAGE_KEY, token)
+    }else{
+      localStorage.removeItem(TOKEN_STORAGE_KEY);
+    }
+
+    if(id){
+      localStorage.setItem(ID_STORAGE_KEY, id);
+    }
+    else{
+      localStorage.removeItem(ID_STORAGE_KEY)
+    }
+  },[token,id])
+
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<LandingPage />}></Route>
+          <Route path="/" element={<LandingPage token={token} id={id} />}></Route>
           <Route path="/login" element={<Login />}></Route>
           <Route path="/signup" element={<SignUp />}></Route>
         </Routes>
--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,11 +1,75 @@
+import { useEffect, useState } from "react";
 import Header from "../components/Header";
 import MainHeading from "../components/MainHeading";
+import axios, { AxiosError } from "axios";
+import { API_URL } from "../App";
 
-const LandingPage = () => {
+const LandingPage = ({
+  token,
+  id
+  }:{
+    token:string | null, 
+    id:string | null;
+  }) => {
+
+  const [username, setUsername] = useState<string>("");
+  const [verified, setVerified] = useState<boolean>(false);
+  const [verifiedCertain, setVerifiedCertain] = useState<boolean>(false);
+
+
+  useEffect(()=>{
+    if (!id) {
+      setVerified(false);
+      setVerifiedCertain(true);
+  }
+  axios
+      .get(`${API_URL}/api/accounts/id/${id}`, {
+          headers: {
+              Authorization: token,
+          },
+      })
+      .then(({ data }) => {
+          setUsername(data.username);
+          setVerified(true);
+          setVerifiedCertain(true);
+      })
+      .catch((e: AxiosError) => {
+          setVerified(false);
+          setVerifiedCertain(true);
+      });
+  },[])
+
+
   return (
     <div className="px-[12px]">
+
+    {verifiedCertain && verified ? (
+                <MainHeading
+                    data={{
+                        username: username,
+                        status: "loggedin",
+                    }}
+                />
+            ) : verifiedCertain === true && verified === false ? (
+                <MainHeading
+                    data={{
+                        status: "not-loggedin",
+                    }}
+                />
+            ) : (
+              <>
+                <MainHeading
+                    data={{
+                        status: "none",
+                    }}
+                />
+                <Header />
+                </>
+            )}
+
+
       <MainHeading />
-      <Header />
+      
     </div>
   );
 };
